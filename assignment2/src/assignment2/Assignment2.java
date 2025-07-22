a364b28cbc0169ff3db0df75944e532b9790c229
Deepak Kumar <67214912+dpk2812002@users.noreply.github.com>
2025-07-22 14:42:11 +0530

    Update Assignment2.java
    Rectify mistake

--- assignment2/src/assignment2/Assignment2.java
+++ assignment2/src/assignment2/Assignment2.java
@@ -81,9 +81,9 @@ public class Assignment2 {
 	int countConsonants(String str) {
 		int noOfConsonants=0;
 		for(int i=0;i<str.length();i++) {
-			if((str.charAt(i)!='a') || (str.charAt(i)!='e')|| (str.charAt(i)!='o')|| (str.charAt(i)!='u') ||
-					(str.charAt(i)!='A') ||(str.charAt(i)!='E') || (str.charAt(i)!='I')|| (str.charAt(i)!='O') || (str.charAt(i)!='U')) {
-				continue;
+			if((str.charAt(i)!='a') && (str.charAt(i)!='e') && (str.charAt(i)!='o') && (str.charAt(i)!='u') &&
+					(str.charAt(i)!='A') && (str.charAt(i)!='E') && (str.charAt(i)!='I') && (str.charAt(i)!='O') && (str.charAt(i)!='U')) {
+				noOfConsonants++;
 			}
 			noOfConsonants++;
 		}
